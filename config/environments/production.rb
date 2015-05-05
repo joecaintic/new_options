6a37920f5527dd7dad11ffd3dcad949a9055aea8
joecaintic <joecaintic@yahoo.com>
2015-05-05 13:39:14 -0400

    changes to production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,5 @@ Rails.application.configure do
 
   #http://bloccit.herokuapp.com
   config.action_mailer.default_url_options = {host: 'http://joecaintic-bloccit.herokuapp.com'}
-   config.action_mailer.delivery_method = :smtp
-   config.action_mailer.perform_deliveries = true
   config.force_ssl = true
 end
