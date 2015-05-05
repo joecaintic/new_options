faa6a736aeced85ac95764266d4dd4bf7e0005e6
joecaintic <joecaintic@yahoo.com>
2015-05-05 13:49:20 -0400

    more production changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #http://bloccit.herokuapp.com
-  config.action_mailer.default_url_options = {host: 'http://joecaintic-bloccit.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'joecaintic-bloccit.herokuapp.com'}
   config.force_ssl = true
 end
