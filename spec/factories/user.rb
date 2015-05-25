774be5742f92fab576968618f258842170449ddf
joecaintic <joecaintic@yahoo.com>
2015-05-25 02:15:35 -0400

    assignment for user with post and comment factory complete

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,5 +5,14 @@ FactoryGirl.define do
      password "helloworld"
      password_confirmation "helloworld"
      confirmed_at Time.now
+     factory :user_with_post_and_comment do
+      transient do
+        count 1
+      end
+      after(:build) do |user, evaluator|
+        create_list(:post, evaluator.count, user: user)
+        create_list(:comment, evaluator.count, user: user)
+    end
+     end
    end
  end
